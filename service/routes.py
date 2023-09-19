b2a73a3fcdcfed76fad08a428461040a8517d04b
guilhermeoliveira-devjourneylog <dev_guilherme_oliveira@outlook.com>
2023-09-19 16:11:49 -0400

    View all customer accounts Review

--- service/routes.py
+++ service/routes.py
@@ -60,7 +60,6 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -70,7 +69,7 @@ def list_accounts():
     app.logger.info("Request to list Accounts") # pylint: disable=no-member
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
-    app.logger.info("Returning [%s] accounts", len(account_list))
+    app.logger.info("Returning [%s] accounts", len(account_list)) # pylint: disable=no-member
     return jsonify(account_list), status.HTTP_200_OK
 
 
@@ -100,8 +99,19 @@ def test_get_account_not_found(self):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """
+    Update an Account
+    This endpoint will update an Account based on the posted data
+    """
+    app.logger.info("Request to update an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    account.deserialize(request.get_json())
+    account.update()
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
